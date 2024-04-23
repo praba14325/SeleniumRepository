2aa7e85d17d66a40f76aa30c3c79dfd29c15cd97
praba14325 <140971375+praba14325@users.noreply.github.com>
2024-04-23 10:42:16 +0530

    Add files via upload
    Day1

---
+++ day1/Firefox.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//browserVersion = 100.2f (use dataType float)
+				//2) browserName = "firefox" (use dataType String)
+				//3) isVisible = true (use dataType boolean)
+				//4) releaseYear = 1998 (use dataType int)
+				//5) browserLogo = 'f' (use datatype char)
+		float browserVersion= 100.2f;
+		String browserName="firefox";
+		boolean isVisible=true;
+		int releaseYear=1998;
+		char browserLogo ='f';
+		System.out.println("browser value:"+browserVersion+"\n"+"Browser Name:"+browserName+"\n"+"IsVisible:"+isVisible+"\n"+"Release Year:"+releaseYear+"\n"+"Browser Logo:"+browserLogo);
+	}
+
+}
---
+++ day1/LearnDataType.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class LearnDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//int intValue=2;float floatValue= 2.2F;double doubleValue=2.222222222222222;char characterValue='D';String stringValue="string";System.out.println("int value:"+intValue);System.out.println("floatValue:"+floatValue+"\n"+"DoubleValue:"+doubleValue);System.out.println(characterValue);System.out.println(stringValue);
+		
+	}
+
+}
